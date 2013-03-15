4b762082d07daf5576365b743b6613fdd3ffb1b7
MailChimp Devs <dev@mailchimp.com>
2013-03-15 17:26:28 +0000

    Add auto_html option to auto-convert plain-text emails to HTML for open and click tracking

--- mandrill.js
+++ mandrill.js
@@ -661,6 +661,7 @@
              - track_opens {Boolean} whether or not to turn on open tracking for the message
              - track_clicks {Boolean} whether or not to turn on click tracking for the message
              - auto_text {Boolean} whether or not to automatically generate a text part for messages that are not given text
+             - auto_html {Boolean} whether or not to automatically generate an HTML part for messages that are not given HTML
              - inline_css {Boolean} whether or not to automatically inline all CSS styles provided in the message HTML - only for HTML documents less than 256KB in size
              - url_strip_qs {Boolean} whether or not to strip the query string from URLs when aggregating tracked URL data
              - preserve_recipients {Boolean} whether or not to expose all recipients in to "To" header for each email
@@ -741,6 +742,7 @@
              - track_opens {Boolean} whether or not to turn on open tracking for the message
              - track_clicks {Boolean} whether or not to turn on click tracking for the message
              - auto_text {Boolean} whether or not to automatically generate a text part for messages that are not given text
+             - auto_html {Boolean} whether or not to automatically generate an HTML part for messages that are not given HTML
              - inline_css {Boolean} whether or not to automatically inline all CSS styles provided in the message HTML - only for HTML documents less than 256KB in size
              - url_strip_qs {Boolean} whether or not to strip the query string from URLs when aggregating tracked URL data
              - preserve_recipients {Boolean} whether or not to expose all recipients in to "To" header for each email
